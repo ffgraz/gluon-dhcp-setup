4b00397cea4a2e18265b08ebdc11bd798e11f4ad
Maciej Krüger <mkg20001@gmail.com>
2023-01-26 23:52:58 +0100

    dont disable dnsmasq

--- post/do-dhcp.sh
+++ post/do-dhcp.sh
@@ -1,6 +1,6 @@
 iptables -A INPUT -p udp --sport 67 --dport 68 -j ACCEPT
 service dnsmasq stop 2>/dev/null || true
-service dnsmasq disable 2>/dev/null || true
+# service dnsmasq disable 2>/dev/null || true
 
 setup_dhcp() { # we're wrapping this in a function so it won't run until it's arrived in one piece
   # find 192.168.1.1
